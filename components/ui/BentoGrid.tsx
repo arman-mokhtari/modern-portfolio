0306e6f4a670a870d645b15934aef57296feaf28
Arman Mokhtari <rmn.mokhtarii@gmail.com>
2024-06-20 11:16:55 +0330

    magic button mobile test

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -11,7 +11,7 @@ import { BackgroundGradientAnimation } from "./GradientBg";
 import GridGlobe from "./GridGlobe";
 import animationData from "@/data/confetti.json";
 import MagicButton from "../MagicButton";
-
+ 
 export const BentoGrid = ({
   className,
   children,
