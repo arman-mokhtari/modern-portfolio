df63817f53c4b11ead37779f1d50287121013178
Arman Mokhtari <rmn.mokhtarii@gmail.com>
2024-06-20 09:35:54 +0330

    hero edited

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -67,7 +67,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "rmn.mokhtarii@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
--- components/ui/TextGenerateEffect.tsx
+++ components/ui/TextGenerateEffect.tsx
@@ -50,7 +50,6 @@ export const TextGenerateEffect = ({
     <div className={cn("font-bold", className)}>
       {/* mt-4 to my-4 */}
       <div className="my-4">
-        {/* remove  text-2xl from the original */}
         <div className=" dark:text-white text-black leading-snug tracking-wide">
           {renderWords()}
         </div>
